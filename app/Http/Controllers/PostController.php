4ab9170c2be9766e58780a754c4e4266a7f8a7ad
Kristian Orozco <korozco@lean-tech.io>
2021-05-23 17:11:56 -0500

    All changes with all signes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
     public function index(){
+        $slug = Str::slug('Your Awesome Blog Title', '-');
         return view('welcome');
     }
 
